09f945b781a233ed0481d0b02aeb677de7f639d3
Rodrigo Okuta <rodrigo.okuta@gmail.com>
2017-01-13 18:34:33 -0200

    Arrumando funcionamento do botao de login do facebook

--- app/src/main/java/br/com/volunapp/volunapp/fragment/LoginFragment.java
+++ app/src/main/java/br/com/volunapp/volunapp/fragment/LoginFragment.java
@@ -1,13 +1,20 @@
 package br.com.volunapp.volunapp.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
+import com.facebook.FacebookAuthorizationException;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 import br.com.volunapp.volunapp.R;
@@ -39,11 +46,40 @@ public class LoginFragment extends Fragment {
 
         loginButton = (LoginButton) view.findViewById(R.id.fragment_login_btn_login);
         loginButton.setFragment(this);
+        loginButton.setReadPermissions("email", "public_profile");
 
 
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast toast = Toast.makeText(getActivity(), "Logged In", Toast.LENGTH_SHORT);
+
+                toast.show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast toast = Toast.makeText(getActivity(), "Canceled", Toast.LENGTH_SHORT);
+
+                toast.show();
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast toast = Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT);
+
+                toast.show();
+            }
+
+        });
+
+
+        return view;
     }
 
-
-
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
