b73a551ed9eb09ead29a36333bbef696e9267f95
leonardosibela <leonardo_sibela@hotmail.com>
2017-01-13 01:34:48 -0200

    Adicionando layout da MainActivity

--- app/src/main/java/br/com/volunapp/volunapp/MainActivity.java
+++ app/src/main/java/br/com/volunapp/volunapp/MainActivity.java
@@ -1,13 +1,44 @@
 package br.com.volunapp.volunapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                break;
+
+            case R.id.action_assignment:
+                break;
+
+            case R.id.action_account:
+                break;
+
+            case R.id.action_settings:
+                break;
+        }
+
+        return true;
     }
 }
