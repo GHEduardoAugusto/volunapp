9f209bbfe1aadc3fe4a1d63f739cd4527ea17558
Rodrigo Okuta <rodrigo.okuta@gmail.com>
2017-01-13 17:37:19 -0200

    Adicionado botão login facebook

--- app/src/main/java/br/com/volunapp/volunapp/MainActivity.java
+++ app/src/main/java/br/com/volunapp/volunapp/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.volunapp.volunapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.ColorRes;
 import android.support.annotation.NonNull;
@@ -10,6 +11,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import br.com.volunapp.volunapp.fragment.AccountFragment;
 import br.com.volunapp.volunapp.fragment.AssignmentFragment;
@@ -24,19 +33,41 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @BindView(R.id.bottom_navigation)
     BottomNavigationView bottomNavigation;
     //
-    @BindView(R.id.activity_main) RelativeLayout rl;
+    @BindView(R.id.activity_main)
+    RelativeLayout rl;
+    //
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        //
+        inicializarVariavel();
+        inicializarAcao();
 
-        rl.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-        bottomNavigation.setVisibility(View.GONE);
-
-        setNavigationBottom();
     }
+
+    private void inicializarVariavel() {
+
+
+    }
+
+    private void inicializarAcao() {
+        //deixando o fundo com a mesma cor devido ao padding
+        rl.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        //tirando o botão, mas quando sair do login ele volta
+        bottomNavigation.setVisibility(View.GONE);
+        //
+        setNavigationBottom();
+
+    }
+
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -65,7 +96,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         return true;
     }
-//
+
+    //
 //    private void setNavigationBottom() {
 //        bottomNavigation.setOnNavigationItemSelectedListener(this);
 //        HomeFragment homeFragment = HomeFragment.newInstance();
--- app/src/main/java/br/com/volunapp/volunapp/fragment/LoginFragment.java
+++ app/src/main/java/br/com/volunapp/volunapp/fragment/LoginFragment.java
@@ -7,6 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
+
 import br.com.volunapp.volunapp.R;
 
 /**
@@ -14,6 +17,10 @@ import br.com.volunapp.volunapp.R;
  */
 
 public class LoginFragment extends Fragment {
+
+    CallbackManager callbackManager;
+    private LoginButton loginButton;
+
 
     public LoginFragment() {
     }
@@ -25,6 +32,18 @@ public class LoginFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) view.findViewById(R.id.fragment_login_btn_login);
+        loginButton.setFragment(this);
+
+
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
+
+
+
 }
